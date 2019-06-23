887d5f277b864359be208cfe3bce7c8ce8af0e32
jreese <jreese@alarm.com>
2019-06-22 20:51:53 -0400

    Add SASS

--- app.js
+++ app.js
@@ -3,11 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var sassMiddleware = require('node-sass-middleware')
 
 var indexRouter = require('./routes/index');
 var twilioRouter = require('./routes/twilio');
 
 var app = express();
+
+//SASS middleware. Ideally, SASS is precompiled to css, but this is easier for dev.
+//See the "compile-sass" build script in package.json, and remember to remove this for release.
+//This version serves css on the fly, compiling it upon request so that changes
+//will be reflected without restarting.  The compiled css should be committed to the repo so this
+//can be easily removed for release.
+app.use(sassMiddleware({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public', 'stylesheets'),
+    prefix:  '/stylesheets', //a request for public/stylesheets/*.css should route back to sass/*.css
+    debug: true
+  })
+);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,7 +33,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/twilio', twilioRouter);
