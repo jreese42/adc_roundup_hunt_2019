0b3ebe76394d3e2e67fa773f38ee30a093ebafa3
jreese <jreese@alarm.com>
2019-06-23 00:49:10 -0400

    Start work on CSS for game info page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('game_info_page', { title: 'Express' });
 });
 
 module.exports = router;
