4d32baf78a2958f58d8c57b4449b542ff6309622
JD Reese <jreese@alarm.com>
2019-07-25 22:50:15 -0400

    basic blog card ui

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 /* eslint-disable no-unused-vars */
 router.get('/', function(req, res, next) {
-    res.render('game_info_page', { title: 'Express' });
+    res.render('blog', { title: 'Express' });
 });
 /* eslint-enable no-unused-vars */
 
