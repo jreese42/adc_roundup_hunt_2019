590d5e59b9531de5001b425dbe644b82de076b4e
JD Reese <jreese@alarm.com>
2019-08-11 23:41:21 -0400

    Add page to full blog entries and rework error page

--- public/js/main.js
+++ public/js/main.js
@@ -1,59 +1,18 @@
 //wow.js
 // new WOW().init();
+const pug = require('pug');
 
-var parallaxElement = document.querySelector('.slide_section');
-var parallaxContainer = parallaxElement.parentNode;
-var containerHeight = parallaxContainer.offsetHeight;
+// // Compile the source code
+// const compiledFunction = pug.compileFile('blog_entry.pug');
 
-/**
- * The speed of the parallax element. Currently ony working
- * with positive values. 
- */
-var parallaxSpeed = 0.5;
+// // Render a set of data
+// console.log(compiledFunction({
+//   name: 'Timothy'
+// }));
+// // "<p>Timothy's Pug source code!</p>"
 
-/**
- * This calculates the new height of the given parallax element.
- * The height is used to fill up the gap between the two sections.
- * It allows the parallax element to move without showing a space.
- * The height is calculated by the given speed
- */
-function setParallaxHeight(element) {
-  var gapHeight = containerHeight - window.innerHeight;
-  var newHeight = 0;
- 
-  if (parallaxSpeed < 0) {
-  	gapHeight = (containerHeight + window.innerHeight) / (1 + parallaxSpeed);
-  }
-  
-  newHeight = containerHeight + gapHeight * Math.abs(parallaxSpeed);
-
-  element.style.height = newHeight + 'px';
-}
-
-/**
- * This simply sets the translation value of the parallax element.
- */
-function updateElement() {
-  var scrollY = window.scrollY;
-  var elementOffset = parallaxElement.getBoundingClientRect();
-  var elementTop = elementOffset.top + scrollY;
-
-  /**
-   * This is the translation value on the y axis. This will start 
-   * the element moving above the lower bound of the viewport after 
-   * the user scrolled to the edge of the element.
-   */
-  var translateY = 0;
-	
-  if (parallaxSpeed < 0) {
-    translateY = parallaxSpeed * (scrollY + window.innerHeight - elementTop);
-  }
-  else {
-  	translateY = parallaxSpeed * (scrollY - elementTop);
-  }
-
-  parallaxElement.style.transform = 'translate3d(0,' + translateY + 'px,0)';
-}
-
-setParallaxHeight(parallaxElement);
-window.onscroll = updateElement;
+// // Render another set of data
+// console.log(compiledFunction({
+//   name: 'Forbes'
+// }));
+// // "<p>Forbes's Pug source code!</p>"
--- routes/index.js
+++ routes/index.js
@@ -49,11 +49,64 @@ router.get('/', function(req, res, next) {
         ]
     };
 
-    res.render('blog', locals);
+    res.render('blog_index', locals);
 });
 
 router.get('/about', function(req, res, next) {
     res.render('game_info_page');
+});
+
+router.get('/blog/entry/:entryId', function(req, res, next) {
+        var blog_posts = [
+            {
+                title: "Does Anyone Have Any Laser Parts?",
+                subtitle: "Asking for a friend",
+                imagePath: "/img/puppy1.jpg", 
+                dateString: "September 27, 2019", 
+                timeString: "10:56 am", 
+                author: "bbg", 
+                teaserText: "I’ve never been to Florida, but it looks like a prime piece of real estate for an evil villain like me to take over. In fact, it might be the perfect state to test my newest laser cutting technology on… I could cut off the peninsula and have my very own private island to develop my evil plans without being interrupted by pesky neighbors. Of course this would be a tremendous undertaking, so I have to put a lot of plans into place to make this happen….  I also need to lock down access to the Death Laser control page, don't want just anyone hacking in and stopping my plan from being set into motion!",
+                isNew: false
+            },
+            {
+                title: "Does Anyone Have Any Laser Parts???",
+                subtitle: "Asking for a friend",
+                imagePath: "/img/puppy1.jpg", 
+                dateString: "September 27, 2019", 
+                timeString: "10:56 am", 
+                author: "bbg", 
+                teaserText: "So the original laser company I was working with on my Super Duper Laser Ray Gun (SDLRG) was acquired by another larger company … a company called Alarm.com. They also acquired the laser parts that I need to complete my SDLRG, so I attempted...",
+                isNew: false
+            },
+            {
+                title: "The Title",
+                subtitle: "The subtitle",
+                imagePath: "/img/puppy1.jpg", 
+                dateString: "Morched 33 2024", 
+                timeString: "98:98 pm", 
+                author: "bbg", 
+                teaserText: "This is the teaser",
+                isNew: true
+            },
+            {
+                title: "First Blog Post Please Ignore",
+                subtitle: "Just testing for now",
+                imagePath: "/img/puppy1.jpg", 
+                dateString: "September 27, 2019", 
+                timeString: "10:56 am", 
+                author: "bbg", 
+                teaserText: "So the original laser company I was working with on my Super Duper Laser Ray Gun (SDLRG) was acquired by another larger company … a company called Alarm.com. They also acquired the laser parts that I need to complete my SDLRG, so I attempted...",
+                isNew: false
+            },
+        ];
+    if (req.params.entryId < blog_posts.length) {
+        var locals = {blog: {}};
+        locals.blog = blog_posts[req.params.entryId];
+        res.render('blog_entry', locals);
+    }
+    else {
+        res.render('error');
+    }
 });
 /* eslint-enable no-unused-vars */
 
