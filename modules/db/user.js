773012c056772f8d6d47e4a54bd9d784343f0904
Samantha <smendis@alarm.com>
2019-08-13 07:03:08 -0400

    updating working copy

--- modules/db/user.js
+++ modules/db/user.js
@@ -1,4 +1,4 @@
-//Init Sequelize for thi file
+//Init Sequelize for this file
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize({
@@ -6,116 +6,105 @@ const sequelize = new Sequelize({
     storage: 'adcHuntDb.sqlite'
   });
 
+  //validating communcation to the database 
   sequelize.authenticate().then(() => {
     console.log('Connection established successfully.');
   }).catch(err => {
     console.error('Unable to connect to the database:', err);
   });
-  /*.finally(() => {
-    sequelize.close();
-  });*/
 
-  var Foo = sequelize.define('foo', {
-    firstname: Sequelize.STRING,
-    lastname: Sequelize.STRING
- 
-  });
-
-
+  //defining the user table columns
   let user = sequelize.define('user', {
     userId: {type: Sequelize.INTEGER, primaryKey: true},
     firstName: Sequelize.STRING,
     lastName: Sequelize.STRING,
-    Puzzle1: Sequelize.BOOLEAN,
-    Puzzle2: Sequelize.BOOLEAN,
+    displayNameFormat: Sequelize.ENUM('FirstName LastName', 'FirstInitialLastName', 'Anonymous'),
+    cookie: Sequelize.STRING,
+    solution1: Sequelize.BOOLEAN,
+    solution2: Sequelize.BOOLEAN,
+    solution3: Sequelize.BOOLEAN,
+    solution4: Sequelize.BOOLEAN,
+    solution5: Sequelize.BOOLEAN,
+    solution6: Sequelize.BOOLEAN,
+    solution7: Sequelize.BOOLEAN,
+    solution8: Sequelize.BOOLEAN,
+    solution9: Sequelize.BOOLEAN,
+    prizeLevel: Sequelize.ENUM('none', 'bluesticker', 'yellowsticker', 'starsticker'),
+    score: Sequelize.INTEGER,
+    hasClaimedSticker: Sequelize.BOOLEAN,
   });
 
+  //adding fake users to the databases
+  //DEV ONLY - REMOVE
   let users = [
-      {userId: 1, firstName: 'Sam', lastName: 'Mendis'},
-      {userId: 2, firstName: 'Bob', lastName: 'TheBuilder'},
-      {userId: 3, firstName: 'Clark', lastName: 'Kent'},
-      {userId: 4, firstName: 'John', lastName: 'Smith'},
-      {userId: 5, firstName: 'Jane', lastName: 'Joe'},
-  ];
-
-  sequelize.sync({force:false}).then(() => {
+      {userId: 1, firstName: 'Sam', lastName: 'Mendis', displayNameFormat:'Anonymous', cookie:"none",  solution1: 0, solution2: 0, solution3: 0, solution4: 0, solution5: 0, solution6: 0, solution7: 0, solution8: 0, solution9: 0,  prizeLevel: 'none',score: 0, hasClaimedSticker: 0},
+      {userId: 2, firstName: 'Bob', lastName: 'TheBuilder',displayNameFormat:'Anonymous', cookie:"none",solution1: 0, solution2: 0, solution3: 0, solution4: 0, solution5: 0, solution6: 0, solution7: 0, solution8: 0, solution9: 0, prizeLevel: 'none',score: 0, hasClaimedSticker: 0},
+      {userId: 3, firstName: 'Clark', lastName: 'Kent',displayNameFormat:'Anonymous', cookie:"none", solution1: 0, solution2: 0, solution3: 0, solution4: 0, solution5: 0, solution6: 0, solution7: 0, solution8: 0, solution9: 0, prizeLevel: 'none',score: 0, hasClaimedSticker: 0},
+      {userId: 4, firstName: 'John', lastName: 'Smith',displayNameFormat:'Anonymous', cookie:"none", solution1: 0, solution2: 0, solution3: 0, solution4: 0, solution5: 0, solution6: 0, solution7: 0, solution8: 0, solution9: 0, prizeLevel: 'none',score: 0, hasClaimedSticker: 0},
+      {userId: 5, firstName: 'Jane', lastName: 'Joe',displayNameFormat:'Anonymous', cookie:"none", solution1: 0, solution2: 0, solution3: 0, solution4: 0, solution5: 0, solution6: 0, solution7: 0, solution8: 0, solution9: 0, prizeLevel: 'none',score: 0, hasClaimedSticker: 0},
+     ];
+  sequelize.sync({force:true}).then(() => {
     user.bulkCreate(users, {validate: true}).then(()=>{ 
     console.log('added row created');
   }).catch((error)=> {
     console.log('failed to create notes');
     console.log(err);
   });
- 
-
-   user.findOne({ where: { userId: 1 } }).then(user => {
-    console.log(user.get({ plain: true }));
-    })
 });
 
-async function countRows() {
-
-    let n = await user.count();
-    console.log(`There are ${n} rows`);
-}
-
-async function deleteRow(id) {
+async function deleteUser(id) {
     let n = await user.destroy({ where: { userId: id } });
     console.log(`number of deleted rows: ${n}`);
 }
-
-
-async function updateRow( firstName) {
-    let id = await user.update(
-        { firstName: firstName.toString() },
-        { where: { userId: 1 } });
+//NOTE: SLM need to create this function 
+async function createUser(fName, lName) {
+  //let n = await user.createUser({ where: { userId: id } });
+  console.log(`created new user: ${n}`);
 }
 
-
 //findUserbyPK
-async function getRow( userId) {
+async function getUserFromDatabase( userId) {
     const row = user.findByPk(userId);
-        //console.log("help!");
         console.log(row.firstName);
     return row;
 }
 
-
-
   module.exports = 
 {
-deleteRow: (userId) => 
+// external function to remove a row
+deleteUser: (userId) => 
     {
-        console.log("hi");
-        deleteRow(userId);
+        console.log("Deleting row");
+        deleteUser(userId);
     },
-countRow: () => 
+// external function to remove a row
+createUser: (firstName, lastName) => 
     {
-        console.log("counting rows");
-        countRow();
+        console.log("creating new user in the database");
+        createUser(firstName, lastName);
     },
-updateUserSolvedPuzzleOne: (userId) => 
+// external functions to get a row from the database
+getUser: (userId) => 
     {
-        console.log("User solved Solution 1");
-        updateRow(userId);
+        console.log("getting user from database");
+        return getUserFromDatabase(userId);
+    },  
+// external function to remove a row
+/*deleteBlogPost: (blogId) => 
+    {
+        console.log("Deleting row");
+        //deleteBlogPost(blogId);
     },
-hi: () => 
+// external function to create a row
+createBlogPost: () => 
     {
-        console.log("help!");
-        return getRow(1);
+        console.log("creating new user in the database");
+        //createBlogPost();
     },
-getUserRealName: (userId) => 
+// external functions to get a row from the database
+getBlogPost: (blogId) => 
     {
-        console.log("help!");
-        var realName = getRow(userId).then( user => {
-            return user.firstName + " " + user.lastName;
-        });
-        return "Name: " + realName;
-    },
-saveUser: (userObj) => 
-    {
-        console.log("saving user");
-        
-    }
-
-    
+        console.log("getting blog post");
+        return getBlogPostFromDatabase(blogId);
+    },*/  
 };
--- routes/dev.js
+++ routes/dev.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var waitUntil = require('wait-until');
-var foo = require('../modules/db/user.js');
+var userObj = require('../modules/db/user.js');
 var router = express.Router();
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) 
 {
-  //foo.deleteRow(1);
-  //foo.countRow();
-  foo.hi().then( user => {
+  userObj.createUser("Andy", "Barron");
+  userObj.getUser(1).then( user => {
     console.log("User " + user.userId + ": " + user.firstName + " " + user.lastName);
     res.send("<h1>" + user.firstName + "</h1>");
     user.firstName = "Samantha";
@@ -17,27 +16,6 @@ router.get('/', function(req, res, next)
       console.log("error saving")
     })
   });
-
-  console.log("test");
-
-  // console.log(user);
-  //initDatabase.printLog();
-  //initDatabase.createDatabase();
-  //var error = initDatabase.databaseConnection();
-  //console.log(error.result);
-  //initDatabase.createUserTable();
-  //initDatabase.addRowToUserTable();
-  //initDatabase.useSql();
-  //user.findAll().then(function(users) {
-  //  console.log(users);
-  //});
-
-  //User.findOne().then(function (user) {
-  //  console.log(user.get('firstName'));
-  //});
-  //console.log("hello world");
-//  console.log(req.query);
-  
 });
 
 module.exports = router;
