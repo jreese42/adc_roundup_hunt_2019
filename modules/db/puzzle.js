3227c7222c782986a3c9dd4e9c914d6a71746837
Toastdeib <lord.xela@gmail.com>
2019-08-21 17:38:50 -0400

    Twiddled password validation to use regexes instead of strings

--- modules/db/puzzle.js
+++ modules/db/puzzle.js
@@ -4,7 +4,7 @@ module.exports = (sequelize, DataTypes) => {
   class Puzzle extends Sequelize.Model {};
   Puzzle.init({
     puzzleId: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true},
-    solutionReference: { type: DataTypes.STRING(255), allowNull: false },
+    solutionRegexReference: { type: DataTypes.STRING(255), allowNull: false },
     solvedCount: { type: DataTypes.INTEGER, defaultValue: 0 },
     incorrectCount: { type: DataTypes.INTEGER, defaultValue: 0 },
 
